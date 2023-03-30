6b0d229ccb74ba6a1dc41b4dbc887b066d170edd
吴海洋 <why@vip.local>
2023-03-30 16:38:04 +0800

    28

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -2,6 +2,17 @@
   <div class="shop">商家信息</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import axios from "../../api/index";
+
+const rst = ref({});
+
+onMounted(async () => {
+  const res = await axios.get("/api/profile/batch_shop");
+  rst.value = res.data.rst;
+  console.log(rst.value);
+});
+</script>
 
 <style></style>
